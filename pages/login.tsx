db46f3268c11933c7cd562a9b8c313527ad9f016
JulieHM <julieholtemotland@hotmail.com>
2023-01-03 13:53:50 +0100

    add a page for login

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Login() {
+  return (
+    <>
+      <p>Logg inn her</p>
+    </>
+  )
+}
