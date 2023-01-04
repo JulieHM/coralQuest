39c8020ee949eeb39e846d1d0c0d124e97cf302f
JulieHM <julieholtemotland@hotmail.com>
2023-01-04 13:06:04 +0100

    Add pages for each quest

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import ts from "typescript";
 
 const Game = () => {
     return (
       <div className="flex py-2 container mx-auto">
-        <p>Game</p>
+        <h1>Game</h1>
+        
+        <Link href={"Quests/1"}></Link>
       </div>
     );
   };
