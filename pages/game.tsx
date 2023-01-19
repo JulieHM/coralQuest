96104cf172a4e540e05a793f5751c2de2ed1b294
JulieHM <julieholtemotland@hotmail.com>
2023-01-19 11:24:16 -0400

    add some css

--- pages/game.tsx
+++ pages/game.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 const Game = () => {
     return (
-      <div className="flex py-2 container mx-auto">
+      <>
         <h1>Game</h1>
         <Link href={"quest/1"}><button>Quest 1 - Quiz</button></Link>
-         <Link href={"quest/2"}><button>Quest 2 - Google Earth</button></Link>
+        <Link href={"quest/2"}><button>Quest 2 - Dykketur</button></Link>
         <Link href={"quest/3"}><button>Quest 3 - hmm?</button></Link>
-      </div>
+      </>
     );
   };
   
--- pages/quest/2.tsx
+++ pages/quest/2.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
 
 export default function Quest2() {
-    return (<><p>Quest 1 - Google earth</p>
-   <iframe src="https://www.google.com/maps/embed?pb=!4v1674137227202!6m8!1m7!1sCAoSLEFGMVFpcE03d3BPeGpBczRGa0xjSUdzUEhBTWs5LUI3aUw3am9QUFFlcVhu!2m2!1d-13.9717939!2d144.5020591!3f296.74272333134803!4f-1.5582225909680574!5f0.7820865974627469" width="600" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-   </>)
-  }
+    return (<>
+    <Link href={"/game"}><p>tilbake</p></Link> 
+    <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
+        <h1>Quest 2 - Dykketur</h1>
+        <iframe src="https://www.google.com/maps/embed?pb=!4v1674137227202!6m8!1m7!1sCAoSLEFGMVFpcE03d3BPeGpBczRGa0xjSUdzUEhBTWs5LUI3aUw3am9QUFFlcVhu!2m2!1d-13.9717939!2d144.5020591!3f296.74272333134803!4f-1.5582225909680574!5f0.7820865974627469" 
+        width="800" height="500" 
+        style={{borderRadius:10, border: "30px solid black"}} 
+        allowFullScreen 
+        loading="lazy" 
+        referrerPolicy="no-referrer-when-downgrade"></iframe><br></br>
+        <textarea style={{borderRadius: 10, width: 850, height: 200}} placeholder="Noter dine observasjoner her ..."></textarea>
+    </div>
+   </>
+   )}
 
 
   //key: AIzaSyARFCFA9CHseihsYaSxfkqritAwj3zIJM4
+
+  //TODO etterhvert: sett input til å være required
+  //TODO: lagre tekststrengen til firebase
+  //TODO: CSS lage dykkerbriller view
+  //TODO: lag tilbake knapp
