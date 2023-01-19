14d8b52e766bb6fd27ce4b765b898e96b7fbcabb
JulieHM <julieholtemotland@hotmail.com>
2023-01-19 09:49:25 -0400

    embedd google maps view into webpage with api key

--- pages/quest/2.tsx
+++ pages/quest/2.tsx
@@ -1,4 +1,14 @@
 
 export default function Quest2() {
-    return <div>Quest 1 - Google earth</div>
+    return (<><p>Quest 1 - Google earth</p>
+    <iframe
+    width="600"
+    height="450"
+    style={{border:0}}
+    loading="lazy"
+    allowFullScreen
+    referrerPolicy="no-referrer-when-downgrade"
+    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyARFCFA9CHseihsYaSxfkqritAwj3zIJM4
+      &q=Space+Needle,Seattle+WA">
+  </iframe></>)
   }
