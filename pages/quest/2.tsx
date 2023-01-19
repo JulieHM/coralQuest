fc2093d55860170fc10c2a598488bc29c295b2b5
JulieHM <julieholtemotland@hotmail.com>
2023-01-19 10:08:51 -0400

    add streetview mode

--- pages/quest/2.tsx
+++ pages/quest/2.tsx
@@ -1,14 +1,9 @@
 
 export default function Quest2() {
     return (<><p>Quest 1 - Google earth</p>
-    <iframe
-    width="600"
-    height="450"
-    style={{border:0}}
-    loading="lazy"
-    allowFullScreen
-    referrerPolicy="no-referrer-when-downgrade"
-    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyARFCFA9CHseihsYaSxfkqritAwj3zIJM4
-      &q=Space+Needle,Seattle+WA">
-  </iframe></>)
+   <iframe src="https://www.google.com/maps/embed?pb=!4v1674137227202!6m8!1m7!1sCAoSLEFGMVFpcE03d3BPeGpBczRGa0xjSUdzUEhBTWs5LUI3aUw3am9QUFFlcVhu!2m2!1d-13.9717939!2d144.5020591!3f296.74272333134803!4f-1.5582225909680574!5f0.7820865974627469" width="600" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+   </>)
   }
+
+
+  //key: AIzaSyARFCFA9CHseihsYaSxfkqritAwj3zIJM4
