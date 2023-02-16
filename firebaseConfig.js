018c0164310f672e31fbb8a0d03778d9c7d3bab0
JulieHM <julieholtemotland@hotmail.com>
2023-02-16 10:23:07 +0100

    add google analytics for sandDollar

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { getDatabase, ref, set, get } from "firebase/database";
+import { getAnalytics, logEvent} from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import firebase from "firebase/app";
@@ -14,23 +15,40 @@ import { getSandDollarValue } from "./components/SandDollar/SandDollar";
 const firebaseConfig = {
   apiKey: "AIzaSyAJgPT9N4sw0k05GaTF71XcFRzaLsv5-r8",
   authDomain: "coral-reef-awareness.firebaseapp.com",
-  databaseURL:
-    "https://coral-reef-awareness-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://coral-reef-awareness-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "coral-reef-awareness",
   storageBucket: "coral-reef-awareness.appspot.com",
   messagingSenderId: "998272556243",
   appId: "1:998272556243:web:776a998a9d4950ede8c826",
-  measurementId: "G-N2W7T6R3SM",
+  measurementId: "G-N2W7T6R3SM"
 };
+
+export let analytics;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 const database = getDatabase();
-//const userRef = database.ref(`users/${userId}`);
+//const userRef = database.rsef(`users/${userId}`);
 //userRef.set({value: 'theValue'})
-/*const analytics = getAnalytics(app);*/
+
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+  console.log(app)
+}
+
+// if (typeof window !== 'undefined') {
+//   logEvent(analytics, "sand_dollar_count", "earn_virtual_currency", {
+//   virtual_currency_name: "Sanddollar",
+//   value: 5
+// });
+// }
+
+
+
+
+
 export default app;
 
 // export function writeUserData(userId, name, email, sandDollarCount) {
@@ -59,3 +77,8 @@ export function writeUserData(
     myCorals: myCorals,
   });
 }
+
+
+
+
+
