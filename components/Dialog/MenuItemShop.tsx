4287862672242206c34ca477a989d8fffe93a640
JulieHM <julieholtemotland@hotmail.com>
2023-02-27 15:48:28 +0100

    add dialog when buying coral

--- components/Dialog/MenuItemShop.tsx
+++ components/Dialog/MenuItemShop.tsx
@@ -5,6 +5,8 @@ import { RoundButton } from "../Button/RoundButton";
 import { context } from "../../pages/_app";
 import { logEvent } from "firebase/analytics";
 import { analytics } from "../../firebaseConfig";
+import { Dialog, DialogTitle, ListItem, List } from "@mui/material";
+import Link from "next/link";
 
 type ShopMenuProps = {
   item: any;
@@ -12,6 +14,16 @@ type ShopMenuProps = {
 };
 
 export const MenuItemShop = ({ item, price }: ShopMenuProps) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const {
     avatarName,
     sandDollarCount,
@@ -24,6 +36,7 @@ export const MenuItemShop = ({ item, price }: ShopMenuProps) => {
     setSandDollarCount(sandDollarCount - price);
     setMyCorals([...myCorals, item]);
     console.log(myCorals);
+    handleClose;
   }
 
   return (
@@ -61,13 +74,62 @@ export const MenuItemShop = ({ item, price }: ShopMenuProps) => {
             href={"/game"}
             title="+"
             onClick={() => {
-              buySandDollar(),
+              setOpen(true),
                 logEvent(analytics, "spend_sand_dollar", {
                   value: sandDollarCount - price,
                   virtual_currency_name: "sand_dollar",
                   item_name: item,
                 });
             }}></RoundButton>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle style={{ backgroundColor: "#A6E0E3" }}>
+              Vil du kjøpe denne korallen?
+            </DialogTitle>
+            <ListItem
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                backgroundColor: "#A6E0E3",
+              }}>
+              <Image
+                src={`/images/corals/display/${item}.svg`}
+                alt={item}
+                width={200}
+                height={200}
+              />
+            </ListItem>
+            <ListItem
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                backgroundColor: "#A6E0E3",
+              }}>
+              <button
+                style={{
+                  border: "none",
+                  color: "#2DAFB8",
+                  padding: "0.5rem 1rem 0.5rem 1rem",
+                  margin: "0.5rem",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleClose()}>
+                Avbryt
+              </button>
+              <button
+                style={{
+                  border: "none",
+                  backgroundColor: "#2DAFB8",
+                  padding: "0.5rem 1rem 0.5rem 1rem",
+                  color: "#ffffff",
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  buySandDollar(), handleClose();
+                }}>
+                Kjøp korall
+              </button>
+            </ListItem>
+          </Dialog>
         </div>
       </div>
     </>
