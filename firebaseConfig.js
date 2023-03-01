7a8aade64915f3c264c2373c40e56af596fbd52f
karendaa <karendaa@stud.ntnu.no>
2023-03-01 15:43:55 +0100

    trying to fix build

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 import { getAuth } from "firebase/auth";
-import { getDatabase, ref, set, get } from "firebase/database";
-import { getAnalytics, logEvent } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import firebase from "firebase/app";
-import "firebase/database";
-import { getSandDollarValue } from "./components/SandDollar/SandDollar";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -29,10 +27,7 @@ export let analytics;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-const database = getDatabase();
-//const userRef = database.rsef(`users/${userId}`);
-//userRef.set({value: 'theValue'})
+export const database = getDatabase();
 
 if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
