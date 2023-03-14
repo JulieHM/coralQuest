6c21e193e93f4121aa19d07655afcfc27fe90da0
Karen Dahl Aarhus <44139426+karendahlaarhus@users.noreply.github.com>
2023-03-14 13:42:47 +0100

    Revert "improve quiz questions and fix gradient on answers"

--- components/Quiz/ButtonWrapper.tsx
+++ components/Quiz/ButtonWrapper.tsx
@@ -17,17 +17,12 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     user-select: none;
     background: ${({ correct, userClicked }) =>
       correct
-        ? "radial-gradient(50% 50% at 50% 50%, rgba(56, 195, 205, 0.774) 0%, rgba(129, 236, 102, 0.396) 0.01%, rgba(98, 227, 96, 0.9) 100%)"
+        ? "radial-gradient(50% 50% at 50% 50%, rgba(54, 253, 74, 0.2) 63.54%, rgba(56, 195, 205, 0.86) 100%)"
         : !correct && userClicked
-        ? "radial-gradient(50% 50% at 50% 50%, rgba(56, 195, 205, 0.774) 0%, rgba(255, 75, 75, 0.396) 0.01%, rgba(228, 65, 65, 0.9) 100%)"
+        ? "radial-gradient(50% 50% at 50% 50%, rgba(255, 73, 73, 0.59) 65.1%, rgba(56, 195, 205, 0.86) 100%)"
         : "radial-gradient(50% 50% at 50% 50%, rgba(54, 239, 253, 0.2) 63.54%, rgba(56, 195, 205, 0.86) 100%)"};
-    border: ${({ correct, userClicked }) =>
-      correct
-        ? "2px solid #7CD28F"
-        : !correct && userClicked
-        ? "2px solid #F36060"
-        : "2px solid #60eaf3"};
-
+    border: 1px solid #aaa;
+    border: 2px solid #60eaf3;
     box-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 60%;
     margin: 3rem;
